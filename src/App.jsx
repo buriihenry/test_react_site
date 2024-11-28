0fe1de5b1ce882d81609a5cfab1a409f29a71fcc
buriihenry <buriihenry@gmail.com>
2024-11-28 09:38:22 +0300

    fixed a bug

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,20 @@ import Home from './pages/Home';
 import {Routes, Route} from "react-router-dom";
 import Favorites from './pages/Favorites';
 import NavBar from './components/NavBar';
+import { MovieProvider } from './contexts/MovieContext';
 
 function App() {
   
   return (
-    <div>
+    <MovieProvider>
       <NavBar />
-    <main className='main-content'>
-      <Routes>
-        <Route path='/'element={<Home />}/>
-        <Route path='/favorites'element={<Favorites />}/> 
-      </Routes>
-
-    </main>
-    </div>
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </MovieProvider>
 
  
   );
